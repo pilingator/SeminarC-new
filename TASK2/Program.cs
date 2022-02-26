92f9664d20c35a16bfe4c8a034020129167c4699
Igor <pilingator86@gmail.com>
2022-02-26 20:48:29 +0700

    дни недели

---
+++ TASK2/Program.cs
@@ -0,0 +1,15 @@
+﻿//Дни недели
+Console.Write("Введите число => ");
+string inputNumber = Console.Readline();
+int number = int.Parse(inputNumber);
+
+If (number == 1) Console.WriteLine("Понедельник"); else
+If (number == 2) Console.WriteLine("Вторник"); else
+If (number == 3) Console.WriteLine("Среда"); else
+If (number == 4) Console.WriteLine("Четверг"); else
+If (number == 5) Console.WriteLine("Пятница"); else
+If (number == 6) Console.WriteLine("Суббота"); else
+If (number == 7) Console.WriteLine("Вскресенье"); else
+{
+    Console.WriteLine("В неделе 7 дней!");
+}
