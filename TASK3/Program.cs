1b9dd33901b07d1e46749b41ef2d41acb9d32ecb
Igor <pilingator86@gmail.com>
2022-02-26 21:07:21 +0700

    функция

---
+++ TASK3/Program.cs
@@ -0,0 +1,7 @@
+﻿// Задача на поиск функции x=f(a)
+Console.Write("Введите число => ");
+string inputNumber = Console.ReadLine();
+int number = int.Parse(inputNumber);
+
+int x = number +11;
+Console.WriteLine($"Функция х = f(a): {x}");
