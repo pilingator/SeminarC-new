9da0bffbd0d1cde04d76a49ca28a2d3a36bae838
Igor <pilingator86@gmail.com>
2022-02-26 20:27:05 +0700

    Sravnenie chisel

---
+++ TASK1/Program.cs
@@ -0,0 +1,15 @@
+﻿//Сравнение чисел 
+Console.Write("Введите число А=> ");
+string inputA = Console.ReadLine();
+int numberA = int.Parse(inputA);
+
+Console.Write("Введите число В=> ");
+string inputB = Console.ReadLine();
+int numberB = int.Parse(inputB);
+
+int value = numberA * numberA;
+if (numberB == value) Console.WriteLine("Да");
+else
+{
+    Console.WriteLine("нет");
+}
