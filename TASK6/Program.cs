662c1e3d0feeaf1c22635628a20e942e82cd4c18
Igor <pilingator86@gmail.com>
2022-02-27 14:36:33 +0700

    commit

---
+++ TASK6/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая выводит случайное число из отрезка [10, 99] и показывает наибольшую цифру числа.
+Random rnd = new Random();
+int value = rnd.Next(10, 99);
+Console.WriteLine($"Случайне число из отрезка 10-99 = {value}");
+int A = value%10; 
+int B = value/10;
+Console.WriteLine(A);
+Console.WriteLine(B);
+if (A>B) Console.WriteLine($"Максимальное число: {A}");
+else 
+Console.WriteLine($"Максимальное число: {B}");
+
