ab089969774945d62e3a7fb44c8f3d9910f5cf2d
Igor <pilingator86@gmail.com>
2022-03-08 17:35:19 +0700

    Проверка числа на четность

---
+++ HW3/Program.cs
@@ -0,0 +1,9 @@
+﻿// Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным 
+// (делится ли оно на два без остатка).
+
+Console.WriteLine("Введите число => ");
+string numberA = Console.ReadLine();
+int A = int.Parse(numberA);
+if (A%2 == 0) Console.WriteLine($"Число {A} четное");
+else 
+Console.WriteLine($"Число {A} не четное");
