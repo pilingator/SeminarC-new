d616b234342e6fc07c175c6dc06aeabafc367891
Igor <pilingator86@gmail.com>
2022-03-08 16:42:58 +0700

    Программа находит квадраты чисел от 1 до N

---
+++ TASK12/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу квадратов чисел от 1 до N
+Console.Write("Введите число => ");
+string inputNumber = Console.ReadLine();
+int N = int.Parse(inputNumber);
+
+int A = 1;
+while (A <= N)
+{
+    Console.WriteLine(Math.Pow(A, 2));
+    A++;
+}
