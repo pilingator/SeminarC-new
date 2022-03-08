444c4f50f4904eea2e4a4666a207faeb75065b4e
Igor <pilingator86@gmail.com>
2022-03-08 17:16:50 +0700

    Отображает все целые числа от -N до N

--- TASK4/Program.cs
+++ TASK4/Program.cs
@@ -1,5 +1,6 @@
 ﻿// Напишите программу, которая на вход принимает одно число (N), а на выходе показывает все целые 
 //числа в промежутке от -N до N.
+
 Console.Write("Введите число => ");
 string inputNumber = Console.ReadLine();
 int N = int.Parse(inputNumber);
