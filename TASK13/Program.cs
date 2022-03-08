005cedf9f670e047964a5bcf3add3bf6132209d6
Igor <pilingator86@gmail.com>
2022-03-08 16:58:20 +0700

    Нахождение третьей цифры заданного числа или сообщение что третьей цифры нет

---
+++ TASK13/Program.cs
@@ -0,0 +1,8 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет 
+Console.Write("Введите число => ");
+string inputNumber = Console.ReadLine();
+int Length = inputNumber.Length;
+
+if (Length > 2) Console.WriteLine($"Третья цифра заданного числа {inputNumber[2]}");
+else 
+Console.WriteLine("Третьей цифры нет");
