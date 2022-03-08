274f70d97fa57b6d2ffcce556b18654be383022e
Igor <pilingator86@gmail.com>
2022-03-08 17:51:30 +0700

    Выводит третью цифру заданного числа или сообщает что ее нет

---
+++ HW7/Program.cs
@@ -0,0 +1,8 @@
+﻿// Задача 3: Напишите программу, которая выводит третью цифру заданного числа или сообщает, 
+// что третьей цифры нет.
+Console.Write("Введите трехзначное число => ");
+string inputNumber = Console.ReadLine();
+int N = int.Parse(inputNumber);
+if (N < 100) Console.WriteLine("Третьей цифры нет");
+else
+Console.WriteLine($"Третья цифра заданного числа => {N%10}");
