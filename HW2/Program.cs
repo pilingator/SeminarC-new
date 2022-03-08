2a4dd8191c6f63edc4eed045509fca2a03e2a50d
Igor <pilingator86@gmail.com>
2022-03-08 17:33:32 +0700

    Нахождение максимального из трех чисел

---
+++ HW2/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 2: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+Console.WriteLine("Введите первое число => ");
+string numberA = Console.ReadLine();
+int A = int.Parse(numberA);
+
+Console.WriteLine("Введите второе число число => ");
+string numberB = Console.ReadLine();
+int B = int.Parse(numberB);
+
+Console.WriteLine("Введите третье число число => ");
+string numberC = Console.ReadLine();
+int C = int.Parse(numberC);
+
+
+if (A > B && A > C) Console.WriteLine($"Число {A} максимальное");
+else 
+if (B > C && B > A) Console.WriteLine($"Число {B} максимальное");
+else 
+if (C > A && C > B) Console.WriteLine($"Число {C} максимальное");
+else 
+Console.WriteLine($"Число {A}, {B}, {C} равны");
