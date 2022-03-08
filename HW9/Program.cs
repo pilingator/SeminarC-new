87c0ae1939309775cf6e25f0573a67ba7636a5c0
Igor <pilingator86@gmail.com>
2022-03-08 18:22:03 +0700

    Нахождение палиндрома из 5 ти значных чисел (строка)

---
+++ HW9/Program.cs
@@ -0,0 +1,11 @@
+﻿// Задача 1: Напишите программу, которая принимает на вход пятизначное число и проверяет, 
+//является ли оно палиндромом.
+
+Console.Write("Введите пятизначное число => ");
+string inputNumber = Console.ReadLine();
+int Length = inputNumber.Length;
+
+if (inputNumber[0] == inputNumber[4] || inputNumber[1] == inputNumber[3]) 
+Console.WriteLine($"Число {inputNumber} является палиндромом");
+else 
+Console.WriteLine($"Число {inputNumber} не является палиндромом");
