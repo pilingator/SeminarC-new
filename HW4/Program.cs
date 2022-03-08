fbd1891c0d676c08398fb6d65612c9543e4b1cc8
Igor <pilingator86@gmail.com>
2022-03-08 17:37:00 +0700

    Находит все четные числа от 1 до N

---
+++ HW4/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 4: Напишите программу, которая на вход принимает число (N), 
+// а на выходе показывает все чётные числа от 1 до N.
+Console.Write("Введите число => ");
+string inputNumber = Console.ReadLine();
+int N = int.Parse(inputNumber);
+
+int A = 1;
+while (A < N)
+{
+   if (A%2 == 0) Console.WriteLine(A);
+
+   A++;
+   
+}
