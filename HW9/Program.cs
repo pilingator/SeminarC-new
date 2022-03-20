6c9f1ea3ddc73753a5e25640d505a33a04927478
Igor <pilingator86@gmail.com>
2022-03-20 13:49:01 +0700

    Проверка пятизначнго числа на полиндром

--- HW9/Program.cs
+++ HW9/Program.cs
@@ -6,7 +6,7 @@ string N = Console.ReadLine();
 int Length = N.Length;
 if (Length > 5 || Length <= 4) Console.WriteLine($"Вы ввели не пятизначное число");
 else 
-if (N[0] == N[4] || N[1] == N[3]) 
+if (N[0] == N[4] && N[1] == N[3]) 
 Console.WriteLine($"Число {N} является палиндромом");
 else 
 Console.WriteLine($"Число {N} не является палиндромом");
