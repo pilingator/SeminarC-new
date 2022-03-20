74255a4b08caef32405a591f51c115c41664e45c
Igor <pilingator86@gmail.com>
2022-03-20 18:52:47 +0700

    Принимает на вход число и выдает сумму чисел от 1 до А

---
+++ TASK18/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая принимает на вход число (А)
+// и выдает сумму числео от 1 до А
+
+int number = Prompt ("Введите число => ");
+Console.WriteLine($"Сумма чисел от 1 до {number} равна {NumberSumm(number)}");
+
+
+int NumberSumm(int digit)
+{
+   int sum = 0;
+for (int i = 1; i <= digit; i++)
+{
+    sum += i;
+} 
+return sum;
+}
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    string strValue = Console.ReadLine();
+    int Value = int.Parse(strValue);
+    return Value;
+}
