430aa14eca2eb5c7d0e01168ab1c39161ab88762
Igor <pilingator86@gmail.com>
2022-03-20 19:25:02 +0700

    !Нахождение суммы чисел от 1 до А (моя версия)!

---
+++ TASK19/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая принимает на вход число (А) и выдаёт сумму чисел от 1 до А.
+
+Console.Write("Введите число => ");
+string inputNumber = Console.ReadLine();
+int N = int.Parse(inputNumber);
+int sum = 0;
+for(int A = 0; A <= N; A++)
+{
+    sum = sum + A;
+}
+Console.WriteLine(sum);
