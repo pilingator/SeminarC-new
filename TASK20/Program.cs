1973dc40cd256710e8405b51463c68e1b697f0af
Igor <pilingator86@gmail.com>
2022-03-20 19:41:06 +0700

    !Принимает на вход число и выдаёт количество цифр в числе (моя версия)!

---
+++ TASK20/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+
+Console.Write("Введите число => ");
+string inputNumber = Console.ReadLine();
+int N = int.Parse(inputNumber);
+int i = 1;
+while(N / 10 != 0)
+{
+    N = N / 10;
+    i++;
+}
+Console.WriteLine($"Количество цифр в числе {inputNumber} равно {i} ");
