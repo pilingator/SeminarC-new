01ada1c496aa9da9cef34884d18d4cb5198fca1a
Igor <pilingator86@gmail.com>
2022-03-20 18:45:34 +0700

    Выводит массив из 8 элементов, заполненный 0 и 1

---
+++ TASK17/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая выводит массив из 8 элементов, заполненный нулями и единицами 
+//в случайном порядке. [1,0,1,1,0,1,0,0]
+
+void FindArray(int[] arr)
+{
+    Random rnd = new Random();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(0, 2);
+    }
+}
+void PrintArray(int[] collect)
+{
+    Console.Write("[");
+    for (int i = 0; i < collect.Length - 1; i++)
+    {
+        Console.Write(collect[i] + ",");
+
+    }
+    Console.WriteLine(collect[7] + "]");
+}
+int[] arr = new int[8];
+
+FindArray(arr);
+PrintArray(arr);
+
+
