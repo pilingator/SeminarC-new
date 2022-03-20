df353fd1bdf4ffad40956ec3e1ffa78e5d4d7ef8
Igor <pilingator86@gmail.com>
2022-03-20 18:32:15 +0700

    Выдает сумму чисел от 1 до А

---
+++ TASK14/Program.cs
@@ -0,0 +1,23 @@
+﻿// Программа принимает на вход А и выдает сумму чисел от 1 до А
+
+int number = Prompt ("Введите число => ");
+Console.WriteLine($"Сумма чисел от 1 до {number} равна {NumberSumm(number)}");
+
+
+int NumberSumm(int digit)
+{
+   int sum = 0;
+for (int i = 1; i <= digit; i++)
+{
+    sum += i;
+} 
+return sum;
+}
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    string strValue = Console.ReadLine();
+    int Value = int.Parse(strValue);
+    return Value;
+}
