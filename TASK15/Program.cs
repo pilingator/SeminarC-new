880968a199f3559e668f75269da13606c7d1d850
Igor <pilingator86@gmail.com>
2022-03-20 18:35:36 +0700

    Генерирует случайное число и выдает количество цифр в числе

---
+++ TASK15/Program.cs
@@ -0,0 +1,25 @@
+﻿// Программа генерирует случайное число и выводит кличество цифр в числе
+//string number = Promt("Введите число => "); 
+//int len = number.Length; 
+string number = Randomize();
+int lenT = number.Length;
+Console.WriteLine($"Количество цифр в числе {number} равно {lenT}");
+
+string Randomize()  
+{
+Random rnd = new Random();
+int digit = rnd.Next();
+string newDigit = digit.ToString();
+return newDigit;
+}
+
+string Prompt(string message)
+{
+    Console.Write(message);
+    string strValue = Console.ReadLine();
+    return strValue;
+}
+
+
+
+
