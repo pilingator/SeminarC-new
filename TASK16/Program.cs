691c9213b42d32f2310e6d6ac2534a67d821061e
Igor <pilingator86@gmail.com>
2022-03-20 18:38:31 +0700

    Находит произведение чисел от 0 до N

---
+++ TASK16/Program.cs
@@ -0,0 +1,31 @@
+﻿// Для введенного числа N находит произведение чисел т 1 д N
+int Prompt(string message)
+{
+    Console.Write(message);
+    string strValue = Console.ReadLine();
+    int Value = int.Parse(strValue);
+    return Value;
+}
+int Proisv(int Value)
+{
+    int fact = 1;
+    if (Value < 1)
+    {
+        for (int i = 1; i >= Value; i--)
+        {
+            fact = fact * i;
+        } 
+    }
+        else if (Value > 1)
+        {
+            for (int i = 1; i <= Value; i++)
+            {
+                fact = fact * i;
+            }
+        }
+return fact;
+}
+    
+    int N = Prompt("Введите число => ");
+    int F = Proisv(N);
+    Console.WriteLine($"Произведение числе от 1 до {N} равно {F}");
